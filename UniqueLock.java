ff3d3280de4c60fc2801d670857572b8e6e858ed
Santhosh Cheeran <40557059+cheerans@users.noreply.github.com>
2019-09-30 12:08:50 -0400

    Update UniqueLock.java

--- UniqueLock.java
+++ UniqueLock.java
@@ -10,25 +10,26 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class UniqueLock {
-	
-	private static Map lockDataMap = new ConcurrentHashMap<String,LockData>();
-	
+
+	private static Map lockDataMap = new ConcurrentHashMap<String, LockData>();
+
 	public static String SERVICENAME1 = "SERVICENAME1";
 	public static String SERVICENAME2 = "SERVICENAME2";
 	public static String SERVICENAME3 = "SERVICENAME3";
 	public static String SERVICENAME4 = "SERVICENAME4";
-	
-	
-	public class LockData {
-		
-        // Creates a random access file stream to read from, and optionally to write to
-        private FileChannel channel = null;
 
-        // Acquire an exclusive lock on this channel's file (blocks until lock can be retrieved)
-        private FileLock lock = null;
+	public class LockData {
+
+		// Creates a random access file stream to read from, and optionally to
+		// write to
+		private FileChannel channel = null;
+
+		// Acquire an exclusive lock on this channel's file (blocks until lock
+		// can be retrieved)
+		private FileLock lock = null;
 
 		public LockData(FileChannel channel, FileLock lock) {
-			
+
 			super();
 			this.channel = channel;
 			this.lock = lock;
@@ -44,50 +45,48 @@ public class UniqueLock {
 	}
 
 	public static boolean getLock(String serviceName) {
-        
+
 		boolean bRet = false;
-        try {
-             
-            File file = new File(serviceName + ".dat");
-             
-            // Creates a random access file stream to read from, and optionally to write to
-            FileChannel channel = new RandomAccessFile(file, "rw").getChannel();
- 
-            // Acquire an exclusive lock on this channel's file 
-            // (blocks until lock can be retrieved)
-            FileLock lock = channel.lock();
- 
-            // Attempts to acquire an exclusive lock on this channel's 
-            // file (returns null or throws an exception if the file 
-            // is already locked.
-            try {
-         
-            	lock = channel.tryLock();
-            	bRet = true;
-            	UniqueLock.LockData storeFileLockData = new UniqueLock().new LockData(channel, lock);
-            	lockDataMap.put(serviceName, storeFileLockData);
-            }
-            catch (OverlappingFileLockException e) {
-            	bRet = false;
-            }
-        }
-        catch (IOException e) {
-        	bRet = false;
-        }         
-        return bRet;
-    }
-	
+		try {
+
+			File file = new File(serviceName + ".dat");
+
+			// Creates a random access file stream to read from, and optionally
+			// to write to
+			FileChannel channel = new RandomAccessFile(file, "rw").getChannel();
+
+			// Acquire an exclusive lock on this channel's file
+			// (blocks until lock can be retrieved)
+			FileLock lock = channel.lock();
+
+			// Attempts to acquire an exclusive lock on this channel's
+			// file (returns null or throws an exception if the file
+			// is already locked.
+			try {
+
+				lock = channel.tryLock();
+				bRet = true;
+				UniqueLock.LockData storeFileLockData = new UniqueLock().new LockData(channel, lock);
+				lockDataMap.put(serviceName, storeFileLockData);
+			} catch (OverlappingFileLockException e) {
+				bRet = false;
+			}
+		} catch (IOException e) {
+			bRet = false;
+		}
+		return bRet;
+	}
+
 	public static void releaseLock(String serviceName) {
-        
-        try {        	
-        	UniqueLock.LockData storeFileLockData = (UniqueLock.LockData)lockDataMap.get(serviceName);          
-            // release the lock
-            storeFileLockData.getLock().release();
-            // close the channel
-            storeFileLockData.getChannel().close();             
-        }
-        catch (IOException e) {
-            System.out.println("I/O Error: " + e.getMessage());
-        }         
-    }
+
+		try {
+			UniqueLock.LockData storeFileLockData = (UniqueLock.LockData) lockDataMap.get(serviceName);
+			// release the lock
+			storeFileLockData.getLock().release();
+			// close the channel
+			storeFileLockData.getChannel().close();
+		} catch (IOException e) {
+			System.out.println("I/O Error: " + e.getMessage());
+		}
+	}
 }
